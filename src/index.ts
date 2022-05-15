372067c769e6f6b303ceb52a612c28b45d56558c
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-05-15 15:10:01 +0100

    feat : Setup typescript environment

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from 'express'
+
+const middleware = (err: any, request: Request, response: Response, next: NextFunction) : void => {
+
+}
+
+export default middleware
